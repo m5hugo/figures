dadd70ebf5c4ccf797901b79af1e3fc2c87cf995
hugo <sinacabar@gmail.com>
2018-04-09 14:14:03 +0200

    Inici de projecte

---
+++ figures/src/figures/Circulo.java
@@ -0,0 +1,20 @@
+package figures;
+
+public class Circulo 
+{
+	private int radi;
+	
+	public Circulo(int radi) throws Exception
+	{
+		if ((radi <=0) || ((radi % 1) != 0))
+		{
+			throw new Exception("El radi ha de ser un enter > ó = a 0");
+		}
+		else
+		{
+			this.radi = radi;
+		}
+	}
+
+	
+}
