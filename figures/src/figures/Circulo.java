daac76c2cd380192d78767e5109d61cf1e67eac5
cfgs <cfgs@pc-info-host>
2018-04-09 15:55:26 +0200

    commit añadir getters/setters

--- figures/src/figures/Circulo.java
+++ figures/src/figures/Circulo.java
@@ -1,9 +1,9 @@
 package figures;
 
-public class Circulo 
+public class Circulo
 {
 	private int radi;
-	
+
 	public Circulo(int radi) throws Exception
 	{
 		if ((radi <=0) || ((radi % 1) != 0))
@@ -16,5 +16,21 @@ public class Circulo
 		}
 	}
 
-	
+	public int getRadi() {
+		return radi;
+	}
+
+	public void setRadi(int radi) throws Exception {
+
+		if ((radi <=0) || ((radi % 1) != 0))
+		{
+			throw new Exception("El radi ha de ser un enter > ó = a 0");
+		}
+		else
+		{
+			this.radi = radi;
+		}
+	}
+
+
 }
